ecf22338309c9429a3a47369c9a483e35a1f5fb0
Jackie <jackie.huynh@hotmail.com>
2019-12-18 02:19:35 -0500

    Update coursescraper.py
    - Split course codes into a dictionary (faculty, subject, code, credit amount)
    - Clean up course code strings to prepare for storage into a database

--- york_scraper/spiders/coursescraper.py
+++ york_scraper/spiders/coursescraper.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 
-fa = 'faculty' # LE
-sj = 'subject' # EECS
-cn = 'course number' # 2011
-cr = 'credit amount' # 3.00
-ay = 'academic year' # 2019
-ss = 'study session' # FW
+import os.path
+import csv
+import scrapy
+import re
 
-link = 'https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm.woa/wa/crsq?fa='+ fa + '&sj=' + sj + '&cn=' + cn + '&cr=' + cr + '&ay=' + ay + '&ss=' + ss
+fa = 'faculty'  # LE
+sj = 'subject'  # EECS
+cn = 'course number'  # 2011
+cr = 'credit amount'  # 3.00
+ay = 'academic year'  # 2019
+ss = 'study session'  # FW
+
+link = 'https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm.woa/wa/crsq?fa=' + \
+    fa + '&sj=' + sj + '&cn=' + cn + '&cr=' + cr + '&ay=' + ay + '&ss=' + ss
 # 'https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm.woa/wa/crsq?fa=LE&sj=EECS&cn=1019&cr=3.00&ay=2019&ss=FW
 
-import scrapy
-import csv
-import os.path
-
 # Gets list of course names and it's course code
-class coursescraperSpider(scrapy.Spider):
-    csv_location = "../csv/"
-    name = "course_links"
+class CourseScraper(scrapy.Spider):
+    csv_location = "csv/"
+    name = "course_codes"
 
     def start_requests(self):
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
         file_name = self.csv_location + "subjects.csv"
         urls = []
 
@@ -32,14 +35,16 @@ class coursescraperSpider(scrapy.Spider):
             for data in reader:
                 faculty = data[2]
                 subject = data[0]
-                url = "https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm.woa/wa/crsq1?faculty=" + faculty + "&subject=" + subject + "&academicyear=" + year + "&studysession=" + session
+                url = "https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm.woa/wa/crsq1?faculty=" + \
+                    faculty + "&subject=" + subject + "&academicyear=" + \
+                    year + "&studysession=" + session
                 urls.append(url)
 
         for url in urls:
             yield scrapy.Request(url=url, headers=headers, callback=self.parse)
 
     def parse(self, response):
-        # default mode to set to file is to write 
+        # default mode to set to file is to write
         file_mode = "w"
         file_name = self.csv_location + "courseinfo.csv"
 
@@ -49,21 +54,36 @@ class coursescraperSpider(scrapy.Spider):
         else:
             file_mode = "w"
 
-        # Courses Main site put these in a separate crawler
-        url = "https://w2prod.sis.yorku.ca"
-
-        # prints course code and credits in an array, ex: AP/ADMS 1500 3.00
-        course_list = response.css('td[width="16%"]::text').getall()
+        # parses course code and name ex: AP/ADMS 1500 3.00
+        course_codes = response.css('td[width="16%"]::text').getall()
         course_names = response.css('td[width="24%"]::text').getall()
-        course_site = response.css('td[width="30%"]').css('a::attr(href)').getall() 
+        #course_site = response.css('td[width="30%"]').css('a::attr(href)').getall()
 
-        with open(file_name, mode=file_mode, newline='') as file:
+        with open(file_name, mode=file_mode, encoding="utf-8", newline='') as file:
             writer = csv.writer(file)
 
-            for (code,name,site) in zip(course_list, course_names, course_site):
-                writer.writerow([code, name, url+site])
-                # course['name'] = code + " " + name
-                # course['link'] = url + site
-                # yield {
-                #     'name' : code + " " + name + " link: " + url + site
-                # }
+            for (code, name) in zip(course_codes, course_names):
+                
+                course_dict = self.parse_course_and_subject_code(code)
+                #writer.writerow([code, name])
+                writer.writerow([course_dict['faculty'],
+                                 course_dict['subject'],
+                                 course_dict['code'],
+                                 course_dict['credit'], name])
+
+    def parse_course_and_subject_code(self, course):
+        bar = re.sub('\s+', ' ', course).strip()
+        course_arr = bar.split(" ")
+
+        arr = course_arr[0].split("/")
+        faculty = arr[0]
+        course_subject = arr[1]
+        course_code = course_arr[1]
+        credit_amount = course_arr[2]
+
+        course_dict = {'faculty': faculty,
+                       'subject': course_subject,
+                       'code': course_code,
+                       'credit': credit_amount}
+
+        return course_dict
