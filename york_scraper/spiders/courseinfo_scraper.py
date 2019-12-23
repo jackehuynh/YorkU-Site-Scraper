6bcc9ab58afc3335e716d6b69fd05c6f8f977b4c
Jackie <jackie.huynh@hotmail.com>
2019-12-22 21:07:32 -0500

    Update courseinfo_scraper.py
    - added skeleton of the JSON/Dictionary object
    - added css and xpath selectors to extract the required course informations however still missing the 'notes' and properly extracting the instructor section.

--- york_scraper/spiders/courseinfo_scraper.py
+++ york_scraper/spiders/courseinfo_scraper.py
@@ -61,14 +61,14 @@ class CourseInfoScraper(scrapy.Spider):
 
         # extract all text within <p> tags in the outer table body containing all the inner tables with the course info
         p_text = table_body.css('p::text').getall()
-
-        course_description = p_text[1]
+        course_description = p_text[-3]
 
         #course_info_body = table_body.css("table[width='100%']").get()
 
         # extracts html element (red bar) containing term and section
-        terms = table_body.css("td[width='50%']").css("b::text").getall()
-        sections = table_body.css("td[width='50%']").css("font::text").getall()
+        t_s = table_body.css("td[width='50%']")
+        terms = [string.strip() for string in t_s.css("b::text").getall()]
+        sections = [string.strip() for string in t_s.css("font::text").getall()]
 
         # extracts the table below the term/section containing times, location, cat #, etc.
         info_tables = table_body.css("table[border='2']").css("td[valign='TOP']::text")
@@ -76,13 +76,58 @@ class CourseInfoScraper(scrapy.Spider):
         # xpath version of above
         test = response.xpath('//table[@border="2"]//tr')
 
+        # currently testing
+        test = response.xpath('//table[@border="2"]').xpath('//td[@width="20%" and @valign="TOP"]')
+
         # grabs the entire table holding the courses
         body = response.css("table[border='2']")
 
-        # returns array of days and the location corresponding to those days
-        day = body[i].css("td[width='15%']::text").getall()
-        loc = body[i].css("td[width='45%']::text").getall() # ['LAS C', 'LAS C']
+        course_info_list = []
 
+        # loop through main course tables
+        for i in range(0, len(body)):
+            
+            # catalogue number
+            cat = body[i].css("td[width='20%']").getall()[-2]
+            cat_list = re.sub('<td width=\"20%\" valign=\"TOP\">', '', cat).strip('<br></td>').strip().split('<br>')
+            cat_list = [string.replace('\xa0', '').strip() for string in cat_list] # parsed version, ready to be put into JSON object
+
+            course_type_list = body[i].css("td[width='10%']::text").getall()
+
+            # parse instructor list
+            instructors = body[i].css("td[width='15%']").css("a::text").getall()
+            parsed_instructors = [string.replace('\xa0',' ') for string in instructors]
+
+            # parse times
+            times = body[i].css("td[width='35%']")
+            times.pop(0)    # first element contains the headers of the table (Type, Days, Start Time, etc.)
+
+            # loop through each row of the course table
+            for (time, c_type) in zip(times, course_type_list):
+                #time_info = time.css("td::text").getall()
+                time_info_list = time.css("tr").css
+
+                # this is for when one row has multiple classes on different days/times/locations ie. M/W/F 1:30-2:30 @ LAS A/B/C
+                for info in time_info_list:
+                    time_info = info.css("td::text").getall()
+                    time_info = [string.replace('\xa0','').strip() for string in info]
+                    day = time_info[0]
+                    start_time = time_info[1]
+                    duration = time_info[2]
+                    location = time_info[3]
+                   
+                course_type = c_type
+
+                # cat element is right after time_info element
+                cat_element = time.xpath('following-sibling::td[@width="20%" and @valign="TOP"]')
+                cat_list = time.xpath('string(following-sibling::td[@width="20%" and @valign="TOP"])').getall()
+                cat_list = [string.replace('\xa0', '').strip() for string in cat_list]
+
+                if cat_list.count('') > 0:
+                    # When the Cat # column has nothing in it
+                    cat_list = ['N/A']
+
+               
         # example used is EECS 1019
         cat = body[i].css("td[width='20%']").getall()[-2] # need to clean up
         cat_id = re.sub('<td width=\"20%\" valign=\"TOP\">', '', cat).strip() # 'Y21A01 (LE EECS) <br>E10C01 (SC MATH) <br></td>
@@ -91,10 +136,12 @@ class CourseInfoScraper(scrapy.Spider):
         parsed_cat = [string.replace('\xa0','').strip() for string in cat_id] # ['Y21A01 (LE EECS)', 'E10C01 (SC MATH)']
 
         instructors = body[i].css("td[width='15%']").css("a::text").getall() # ['Andranik\xa0Mirzaian']
-        times = body[i].css("td[width='35%']").getall() # ['time_slot']
-        time_slots = times[1].css("tr").getall() # ['day1', 'day2']
+        times = body[i].css("td[width='35%']") # ['time_slots']
+        time_slots = times[1].css("tr") # ['day1', 'day2']
         day_1 = time_slots[0].css("td::text").getall() # M 17:30 90 LAS\xa0C 
         day_2 = time_slots[1].css("td::text").getall() # R 17:30 90 LAS\xa0C
+
+        course_type = body[i].css("td[width='10%']::text").get()
 
         # removing the '\xa0' string from the parsed text
         parsed_instructors = [string.replace('\xa0',' ') for string in instructors] # ['Andranik Mirazaian']
@@ -102,22 +149,31 @@ class CourseInfoScraper(scrapy.Spider):
         # TODO: probably turn these list comprehensions into functions 
         parsed_day = [string.replace('\xa0', '').strip() for string in day_1] # ['M', '17:30', '90', 'LAS C']
         parsed_section = [string.replace('\xa0', '').strip() for string in sections] # ['Section A', 'Section B', 'Section C', etc]
-
         
         '''
         TODO: add info_dict to scrapy's item feature
         '''
 
         '''
-        nested = []
+        inst_list = []
         # for loop
-        nested_dict = {'day': day,
-                       'start': start,
-                       'duration': duration,
-                       'location': location}
-        # nested.append(nested_dict)
+        nested_dict = {'term': term,
+                       'section': section,
+                       'times': {
+                            'course_type': course_type,
+                            'day': day,
+                            'start': start,
+                            'duration': duration,
+                            'location': location,
+                            'cat': [cat_list],
+                            'instructors': [inst_list],
+                            'notes': notes
+                       }
+                     }
 
-        nested_inst = ['inst1, inst2, etc.']
+        inst_list.append('inst1')
+
+        nested_dict['instructors'] = nested_inst
         '''
 
         '''
@@ -128,8 +184,8 @@ class CourseInfoScraper(scrapy.Spider):
                   'subject': subject,
                   'credit': credit,
                   'academic_year' : year
-                  'Pre-requisites': yes or no, if yes have a list of the courses,
-                  'info': ['info_dict']
+                  'Pre-requisites': yes or no, if yes have a list of the courses of the pre-req # more of a TO-DO instead of an immediate need
+                  'info': ['nested_dict', 'nested_dict', etc.]
                   }
         '''
 
@@ -137,8 +193,11 @@ class CourseInfoScraper(scrapy.Spider):
         info_dict = {
                      'Type': lect,
                      'Time': nested_dict,
-                     'cat': cat,
+                     'cat': cat_list,
                      'instructor': nested_inst,
                      'notes': note 
                      }
         '''
+        # returns array of days and the location corresponding to those days
+        #day = body[i].css("td[width='15%']::text").getall()
+        #loc = body[i].css("td[width='45%']::text").getall() # ['LAS C', 'LAS C']
