21abe826088b8968a29dd2db9d6a00997736f23f
Jackie <jackie.huynh@hotmail.com>
2019-12-26 19:13:43 -0500

    Added item classes for the respective spiders

--- york_scraper/items.py
+++ york_scraper/items.py
@@ -7,8 +7,27 @@
 
 import scrapy
 
+class YorkSubjectItem(scrapy.Item):
+    code = scrapy.Field()
+    name = scrapy.Field()
+    faculty = scrapy.Field()
 
-class YorkScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+# Just has the basic info of a course (name, faculty, subject, etc.)
+class YorkCourseItem(scrapy.Item):
+    faculty = scrapy.Field()
+    subject = scrapy.Field()
+    code = scrapy.Field()
+    credit = scrapy.Field()
+    name = scrapy.Field()
+    url = scrapy.Field()
+
+class YorkCourseInfoItem(scrapy.Item):
+    '''
+    TODO: Add fields for prerequisites, academic year, and LOI (Language of Instructions)
+    '''
+    name = scrapy.Field()
+    description = scrapy.Field()
+    faculty = scrapy.Field()
+    subject = scrapy.Field()
+    credit = scrapy.Field()
+    offerings = scrapy.Field()
